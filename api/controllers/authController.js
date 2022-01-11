51c9aa7a2e7be50b7d54ed2082beaa8c99d3f243
ohjinseo <ojs835@naver.com>
2022-01-11 20:00:10 +0900

    Fix express 비동기 에러처리

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,16 +1,32 @@
 const User = require("../models/User");
+const expressAsyncHandler = require("express-async-handler");
 
 const authController = {
-  register: async (req, res) => {
+  register: expressAsyncHandler(async (req, res) => {
+    const { email } = req?.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      throw new Error("유저가 존재합니다");
+    }
+
     try {
       const newUser = await User.create(req.body);
       res.status(200).json(newUser);
     } catch (error) {
       res.status(500).json(error);
     }
-  },
+  }),
 
-  login: async (req, res) => {},
+  login: async (req, res, next) => {
+    const { email } = req?.body;
+    const user = await User.findOne({ email });
+    if (user) {
+      next(new Error("유저가 존재합니다"));
+    }
+
+    try {
+    } catch (error) {}
+  },
 };
 
 module.exports = authController;
--- api/middlewares/errorMiddleware.js
+++ api/middlewares/errorMiddleware.js
@@ -1,20 +1,10 @@
-const User = require("../models/User");
-
-const userExistHandler = async (req, res, next) => {
-  const { email } = req?.body;
-  const user = await User.findOne({ email });
-  if (user) {
-    next(new Error("유저가 존재합니다"));
-  } else next();
-};
-
 const errorHandler = (error, req, res, next) => {
-  console.log(res.statusCode);
   const errorStatusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(errorStatusCode);
   res.json({
     message: error.message,
+    error: error.stack,
   });
 };
 
-module.exports = { userExistHandler, errorHandler };
+module.exports = { errorHandler };
